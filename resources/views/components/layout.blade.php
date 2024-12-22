c44cb05aa18bbf0bc386c7bc72cf0a05d5695e3b
NereaPinol33 <nereapinol@iesebre.com>
2024-12-22 18:58:15 +0100

    fix: afegir fitxer javascript al layout

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -7,6 +7,7 @@
     <title>{{ config('app.name') }} - {{ $title ?? 'Welcome' }}</title>
     <link rel="icon" href="/favicon.jpg" type="image/x-icon">
     @vite('resources/css/app.css')
+    @vite('resources/js/app.js')
 </head>
 
 <body class="bg-gray-100 text-gray-800 min-h-screen flex flex-col">
