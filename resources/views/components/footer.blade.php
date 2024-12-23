a21f4253ed0eba497465002fc5e3ca580f1f0efb
NereaPinol33 <nereapinol@iesebre.com>
2024-12-23 17:14:36 +0100

    improve: separar header i footer en components

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="bg-black text-white">
+    <div class="container mx-auto p-6 flex">
+        <p>&copy; {{ date('Y') }} <span class="font-bold">MoviesPHP</span> - Nerea Piñol Monterde</p>
+    </div>
+</footer>
